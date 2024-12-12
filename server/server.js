bea494faa9f39fa312f1844ff8f38c70821fe321
sankarbhadran <sankarbhadranr@gmail.com>
2024-12-12 12:52:45 +0530

    'cores'

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const mainRouter = require("./routes/mainRoutes");
-
+const allowedOrigins = ["https://menuapp-pied.vercel.app"];
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "https://menuapp-pied.vercel.app" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use("/api", mainRouter);
 const PORT = 5000;
 app.listen(PORT, () => {
